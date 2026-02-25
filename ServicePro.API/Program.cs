fdbab03ce7cdf1f862afadc549f99050a6bf2eb6
jasmeet singh <jasmeet.singh@caresmartz360.com>
2026-02-25 14:07:19 +0530

    add

--- ServicePro.API/Program.cs
+++ ServicePro.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using QuestPDF.Infrastructure;
 using ServicePro.Core.Interfaces;
 using ServicePro.Infrastructure.Data;
 using ServicePro.Infrastructure.Repositories;
@@ -8,7 +9,14 @@ using ServicePro.Services;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+// ================= QUESTPDF CONFIG =================
+QuestPDF.Settings.License = LicenseType.Community;
+QuestPDF.Settings.CheckIfAllTextGlyphsAreAvailable = false;
 
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
 // ================= DATABASE =================
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Service")));
@@ -21,8 +29,7 @@ builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICloudinaryService, CloudinaryService>();
 builder.Services.AddScoped<IContactService, ContactService>();
-
-
+builder.Services.AddScoped<IPdfService, PdfService>();
 // ================= JWT AUTH =================
 var jwtKey = builder.Configuration["Jwt:Key"];
 var jwtIssuer = builder.Configuration["Jwt:Issuer"];
---
+++ ServicePro.Core/Interfaces/IPdfService.cs
@@ -0,0 +1,16 @@
+﻿using ServicePro.Core.DTOs;
+using ServicePro.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicePro.Core.Interfaces
+{
+    public interface IPdfService
+    {
+        byte[] GenerateContactPdf(List<ContactResponseDto> contacts);
+
+    }
+}
