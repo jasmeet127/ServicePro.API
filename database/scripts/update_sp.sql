f3883edce2f9f2f8a27cc35546892baba614915d
JASSADHAMMI <jassadhammi@gmail.com>
2026-03-18 15:20:20 +0530

    Add step to execute update_sp.sql with SQLCMD
    Added a step to run database update scripts using SQLCMD.

--- database/scripts/update_sp.sql
+++ database/scripts/update_sp.sql
@@ -1,1 +1,11 @@
+- name: Install SQLCMD
+  run: sudo apt-get install -y mssql-tools unixodbc-dev
 
+- name: Run DB Scripts
+  run: |
+    /opt/mssql-tools/bin/sqlcmd -S $DB_SERVER -U $DB_USER -P $DB_PASS -d $DB_NAME -i database/scripts/update_sp.sql
+  env:
+    DB_SERVER: ${{ secrets.DB_SERVER }}
+    DB_USER: ${{ secrets.DB_USER }}
+    DB_PASS: ${{ secrets.DB_PASS }}
+    DB_NAME: ${{ secrets.DB_NAME }}
